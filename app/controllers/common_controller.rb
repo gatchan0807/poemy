df539cae8dbfd4cd33054ac4f3b8463762060f28
ahaha0807 <b5331@oic.jp>
2017-09-24 13:51:39 +0900

    ランディングページの追加

---
+++ app/controllers/common_controller.rb
@@ -0,0 +1,4 @@
+class CommonController < ApplicationController
+  def landing
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
 
   get 'users/show'
 
+  get 'common/landing'
+
+  root :to => 'common#landing'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
