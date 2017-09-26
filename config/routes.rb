5c5384ebdc5c09d3a78f1386ee6ce1f1078093c8
ahaha0807 <b5331@oic.jp>
2017-09-26 20:55:54 +0900

    リファクタリング

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { :omniauth_callbacks => "omniauth_callbacks" }
+  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks'}
   get 'common/landing'
 
-  root :to => 'common#landing'
+  root to: 'common#landing'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
