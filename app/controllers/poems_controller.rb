3b80dbe93493afb0f2f6abca2a91a95019762bf0
ahaha0807 <b5331@oic.jp>
2017-10-12 03:19:13 +0900

    ポエムコントローラーのモックメソッドとビューを追加して動くようにした

---
+++ app/controllers/poems_controller.rb
@@ -0,0 +1,33 @@
+class PoemsController < ApplicationController
+  def popular_page
+
+  end
+
+  def popular
+
+  end
+
+  def index
+
+  end
+
+  def index_of
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def delete
+
+  end
+
+  def add_reaction
+
+  end
+end
