0ed33c5fd9944015ac08d5acf68169aeca591ee4
ahaha0807 <b5331@oic.jp>
2017-10-12 01:27:12 +0900

    共通コントローラーのモックメソッドを追加

--- app/controllers/common_controller.rb
+++ app/controllers/common_controller.rb
@@ -1,4 +1,28 @@
 class CommonController < ApplicationController
   def landing
   end
+
+  def non_sign_in
+    render html: 'non_sign_in'
+  end
+
+  def signed_in
+    render html: 'signed_in'
+  end
+
+  def about
+    render html: 'about'
+  end
+
+  def support
+    render html: 'support'
+  end
+
+  def post_support
+    render html: 'post_support'
+  end
+
+  def policies
+    render html: 'policies'
+  end
 end
