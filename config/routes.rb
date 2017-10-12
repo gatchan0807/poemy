086036a1f713679ee742a57037d99bf69e4dcd0b
ahaha0807 <b5331@oic.jp>
2017-10-12 19:44:23 +0900

    ルーティングファイルをリファクタリング

--- config/routes.rb
+++ config/routes.rb
@@ -11,21 +11,21 @@ Rails.application.routes.draw do
 
   # poems
   get '/popular', to: 'poems#popular_page', as: 'popular_poems_page'
-  get '/poems/popular', to: 'poems#popular', as: 'popular_poems'
+  get '/poems/popular', to: 'poems#popular', as: 'popular_poems' # api
 
-  get '/poems', to: 'poems#index', as: 'poems'
-  get '/:user_id/poems', to: 'poems#index_of', as: 'poems_with_user'
-  post '/:user_id/poems', to: 'poems#create', as: 'post_poem'
+  get '/poems', to: 'poems#index', as: 'poems' # api
+  get '/:user_id/poems', to: 'poems#index_of', as: 'poems_with_user' # api
+  post '/:user_id/poems', to: 'poems#create', as: 'post_poem' # api
   get '/:user_id/poems/:poem_id', to: 'poems#show', as: 'poem_detail'
-  delete '/:user_id/poems/:poem_id', to: 'poems#delete', as: 'delete_poem'
+  delete '/:user_id/poems/:poem_id', to: 'poems#delete', as: 'delete_poem' # api
   post '/:user_id/poems/:poem_id/reaction', to: 'poems#add_reaction', as: 'add_reaction'
 
   # users
   get '/:user_id', to: 'users#show', as: 'user_detail'
   get '/:user_id/following', to: 'users#following', as: 'following'
   get '/:user_id/follower', to: 'users#follower', as: 'follower'
-  post '/:user_id/follow', to: 'users#follow', as: 'follow'
-  delete '/:user_id/follow', to: 'users#unfollow', as: 'unfollow'
+  post '/:user_id/follow', to: 'users#follow', as: 'follow' # api
+  delete '/:user_id/follow', to: 'users#unfollow', as: 'unfollow' # api
 
   root to: 'common#landing'
 end
