fd91ee6cf1e8db4e0b04080cbf2f85f9bcf524df
ahaha0807 <b5331@oic.jp>
2017-10-25 18:34:33 +0900

    リファクタリング

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,11 +1,11 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def twitter
-    @user = User.from_omniauth(request.env["omniauth.auth"].except("extra"))
+    @user = User.from_omniauth(request.env['omniauth.auth'].except('extra'))
 
     if @user.persisted?
       sign_in_and_redirect @user
     else
-      session["devise.user_attributes"] = @user.attributes
+      session['devise.user_attributes'] = @user.attributes
       redirect_to new_user_registration_url
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {omniauth_callbacks: 'omniauth_callbacks'}
+  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
 
   # commons
   get '/users', to: 'common#landing'
