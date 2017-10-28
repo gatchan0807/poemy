451477a3c42f23cb7afd28f40512eed721a826c9
ahaha0807 <b5331@oic.jp>
2017-10-28 18:58:40 +0900

    #15 の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_ids
+  skip_before_action :verify_authenticity_token
 
   def show
   end
@@ -8,6 +9,20 @@ class UsersController < ApplicationController
   end
 
   def follower
+  end
+
+  def follow
+    unless Follow.where('follow_id = ? AND followed_id = ?', current_user.userid, @id).empty?
+      render json: { message: 'Already you followed.' } and return
+    end
+
+    follow = Follow.new(follow_id: current_user.userid, followed_id: @id)
+
+    if follow.save
+      render json: { message: 'Follow was complete.' } and return
+    else
+      render json: { message: 'Follow was failed' }, status: :bad_request and return
+    end
   end
 
   private
