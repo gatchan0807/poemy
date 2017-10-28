8c90386b14d04846b423eca5857f50c70e1ce4fa
ahaha0807 <b5331@oic.jp>
2017-10-28 20:15:34 +0900

    #16 の実装とフォローAPIのレスポンスコードの書き換え

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
 
   def follow
     unless Follow.where('follow_id = ? AND followed_id = ?', current_user.userid, @id).empty?
-      render json: { message: 'Already you followed.' } and return
+      render json: { message: 'Already you followed.' }, status: :conflict and return
     end
 
     follow = Follow.new(follow_id: current_user.userid, followed_id: @id)
@@ -22,6 +22,20 @@ class UsersController < ApplicationController
       render json: { message: 'Follow was complete.' } and return
     else
       render json: { message: 'Follow was failed' }, status: :bad_request and return
+    end
+  end
+
+  def unfollow
+    follow = Follow.where('follow_id = ? AND followed_id = ?', current_user.userid, @id)
+
+    if follow.empty?
+      render json: { message: 'Already you aren\'t follow.' }, status: :not_found and return
+    end
+
+    if follow.delete(follow.ids) > 0
+      render json: { message: 'Unfollow done.' } and return
+    else
+      render json: { message: 'Unfollow failed' }, status: :bad_request and return
     end
   end
 
