960d69ee481da56fb862bcc05ababd6f1da8adcd
ahaha0807 <b5331@oic.jp>
2017-11-08 21:07:10 +0900

    リアクション済みかを確認するユーティリティメソッドの追加

--- app/controllers/concerns/poem_util.rb
+++ app/controllers/concerns/poem_util.rb
@@ -4,4 +4,17 @@ module PoemUtil
 
     SecureRandom.uuid.delete!('-')
   end
+
+  def self.is_reacted(poem_id, reacted_list)
+    response = false
+
+    reacted_list.each { |reacted|
+      if reacted == poem_id
+        response = true
+      end
+    }
+
+    response
+  end
+
 end
